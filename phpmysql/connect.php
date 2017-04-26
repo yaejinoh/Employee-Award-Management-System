47ceedf07396181146a9459851cdd22c883c7ee0
jonese8 <jonese8@onid.oregonstate.edu>
2017-04-25 21:08:30 -0600

    Update connect.php

--- phpmysql/connect.php
+++ phpmysql/connect.php
@@ -4,13 +4,13 @@
 #$servername = "us-cdbr-azure-east-c.cloudapp.net";
 #$usernameDb = "bd07944a668fd5";
 #$passwordDb = "ee00cbfd";
-#$dbname = "bd07944a668fd5";
+#$dbname = "EmployeeRecognition";
 
 $mysqli = new mysqli($servername, $usernameDb, $passwordDb, $dbname);
 
 if ($mysqli->connect_errno) {
     echo "Error: Database connection error: " . $mysqli->connect_errno . " - "
-    . $mysqli->connect_error;
+    . $mysqli->connect_error;	#
 }
 ?>
 
