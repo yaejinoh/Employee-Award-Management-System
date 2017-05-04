a013deb3d0f947cae1cd99b389bd60fba43daf3b
yaejinoh <ohya@oregonstate.edu>
2017-05-04 04:06:48 -0400

    Update createAward.php

--- userforms/createAward.php
+++ userforms/createAward.php
@@ -194,7 +194,7 @@ if (!isset($_Session['employeeLastName']) && !isset($_SESSION['employeeLoggedIn'
                         JOIN CertType CT ON CT.ctid=A.type
                         JOIN Regions R ON R.rid=A.region
 			WHERE PE.id = '$eid'
-			ORDER BY A.date, A.time;"))){
+			ORDER BY A.datetimestamp;"))){
                           echo "Prepare failed: " . $stmt->errno . " " . $stmt->error;
                         }         
                         if(!$stmt->execute()){
