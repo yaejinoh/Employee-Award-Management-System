ece3dba9496020d3ac31c0271db10fbe98b6b573
yaejinoh <ohya@oregonstate.edu>
2017-05-04 05:09:45 -0400

    Update createAward.php

--- userforms/createAward.php
+++ userforms/createAward.php
@@ -231,6 +231,8 @@ if (!isset($_Session['employeeLastName']) && !isset($_SESSION['employeeLoggedIn'
 			      
 			      
 			echo '<img src="data:image/jpeg;base64,'.base64_encode($signature).'"/>';
+			$stmt->close();      
+			    
 			$cur_date = date("y-m-d");
 			$cur_time = date("h:i:s");
 		      
