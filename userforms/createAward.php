1d27fcc93df0ce3ac4342ca4af71bf4d3014254b
yaejinoh <ohya@oregonstate.edu>
2017-05-05 02:27:47 -0400

    Update createAward.php

--- userforms/createAward.php
+++ userforms/createAward.php
@@ -230,6 +230,9 @@ if (!isset($_Session['employeeLastName']) && !isset($_SESSION['employeeLoggedIn'
 			if(!$stmt->bind_result($signature)){
                           echo "Bind failed: " . $stmt->errno . " " . $stmt->error;
                         } 
+			while($stmt->fetch()){
+                          echo '<img src="data:image/png;base64,'.base64_encode($signature).'">';
+	                }
 			$stmt->close();      
 			$cur_date = date("y-m-d");
 			$cur_time = date("h:i:s");
