ec1d8bb6bc12d14dbbbc2041796bff815f3060d1
jonese8 <jonese8@onid.oregonstate.edu>
2017-05-05 16:55:27 -0600

    Add password restore with PHPMailer

--- adminforms/restore.php
+++ adminforms/restore.php
@@ -1,3 +1,99 @@
+<?php
+#restore.php - CS467, Emmalee Jones, Yae Jin Oh
+#User Restore Menu
+#Error Reporting Settings
+include "../phpmysql/connect.php";
+require '../PHPMailer/PHPMailerAutoload.php';
+
+$form_email = '';
+//$error_msg[] = '';
+//Set edit flag
+$passedEdits = TRUE;
+
+if (!empty($_POST)) {
+
+    $emailaddr = $_POST["emailaddr"];
+
+    //echo $passedEdits . "  " . "valid email";
+
+    if (filter_var($emailaddr, FILTER_VALIDATE_EMAIL) === false) {
+        $error_msg[] = "Please enter a valid email.";
+        $passedEdits = FALSE;
+        // echo $passedEdits . "  " . "invalid email";
+    }
+
+
+    if ($passedEdits == TRUE) {
+        /* Test for Duplicate username */
+        if (!($stmt = $mysqli->prepare("SELECT emailaddress, password, firstname, lastname FROM employees WHERE emailaddress=? "))) {
+            $error_msg[] = "Error: Failed prepare: (" . $mysqli->errno . ") " . $mysqli->error;
+//$passedEdits = FALSE;
+        }
+
+        if (!$stmt->bind_param("s", $emailaddr)) {
+            $error_msg[] = "Error: Failed Bind: (" . $stmt->errno . ") " . $stmt->error;
+//$passedEdits = FALSE;
+#echo $passedEdits . "  " . " failed bind ";
+        }
+
+        if (!$stmt->execute()) {
+            $error_msg[] = "Error: Failed execute: (" . $stmt->errno . ") " . $stmt->error;
+//$passedEdits = FALSE;
+        }
+
+        $tabemailaddr = NULL;
+        $tabFirstName = NULL;
+        $tabLastName = NULL;
+        $tabPassword = NULL;
+
+        if (!$stmt->bind_result($tabemailaddr, $tabPassword, $tabFirstName, $tabLastName)) {
+            $error_msg[] = "Error: Failed bind_result: (" . $stmt->errno . ") " . $stmt->error;
+//$passedEdits = FALSE;
+        }
+
+        if (!$stmt->fetch()) {
+
+//$form_email = htmlentities($_POST["emailaddr"]);
+            $error_msg[] = "This account does not exist. Try again.";
+        } else {
+
+
+            // echo $tabemailaddr . "</br>";
+            // echo $tabFirstName . "</br>";
+            // echo $tabLastName . "</br>";
+            // echo $tabPassword . "</br>";
+            $tabFullName = $tabFirstName . " " . $tabLastName;
+
+            // echo $tabFullName . "</br>";
+            date_default_timezone_set('America/Los_Angeles');
+
+            $mail = new PHPMailer;
+            $mail->isSMTP();
+            $mail->SMTPDebug = 0;
+            $mail->Debugoutput = 'html';
+            $mail->Host = 'smtp.gmail.com';
+            $mail->Port = 587;
+            $mail->SMTPSecure = 'tls';
+            $mail->SMTPAuth = true;
+            $mail->Username = "delphinusstate@gmail.com";
+            $mail->Password = "test123$";
+            $mail->setFrom('delphinusstate@gmail.com', 'Employee Recognition');
+            $mail->addAddress($tabemailaddr, $tabFullName);
+            $mail->Subject = 'Delphinus Employee Recognition - Password Recovery';
+            $mail->isHTML(true);
+            $mail->Body = "This is your password: " . $tabPassword;
+            if (!$mail->send()) {
+                $error_msg[] = "Error: " . $mail->ErrorInfo;
+            } else {
+                $error_msg[] = "Email: " . $tabemailaddr . " password sent.";
+            }
+        }
+
+        $stmt->close();
+    }
+}
+$mysqli->close();
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -41,50 +137,62 @@
         <!-- --------------------------------- Restore Form --------------------------------- -->
         <div class="container" >
             <h1>Password Recovery</h1>
-            <form method="POST" onsubmit="editdata();
-                    return false;">
-                <label for="username" class="control-label">Email Address</label>
-                <input name="username" type="text" class="form-control" id="usernamer" placeholder="Username(Email Address)" required>
+            <form method="POST" action="restore.php">
+                <label for="emailaddr" class="control-label">Email Address</label>
+                <input name="emailaddr" type="text" class="form-control" id="emailaddr" placeholder="Username(Email Address)" required>
                 </br>
-                <button type="submit" name="usersignin" class="btn btn-sm btn-primary ">Submit</button> 
-             </form> 
+                <button type="submit" name="recovery" class="btn btn-sm btn-primary ">Submit</button> 
+            </form> 
             </br>
             <a href="../index.php">User Sign In</a>
             </br>
-            <a href="adminSignIn.php">Admin Sign In</a>
 
-        </div>
-        <div class="container">
-            <div class="row">   
-                <div class="col-sm-4"></div> 
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4" style="color:#006600" id="signed_message"></div>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-            </div>     
-        </div>
-        <!-- --------------------------------- Footer --------------------------------- -->
-        <footer class="blog-footer">
-            <p>Powered by <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a></p>
-        </footer>
-        <script src="../js/bootstrap.min.js"></script>
+            <div class="row">
+                <div class="col-sm-6" style="color:#FF0000"</div>
 
-    </body>
+                <?php
+                if (isset($error_msg)) {
+                    foreach ($error_msg as $message) {
+                        echo $message . "<br/>";
+                    }
+                }
+                ?>  
+
+            </div>
+        </div>    
+
+    </div>
+    <div class="container">
+        <div class="row">   
+            <div class="col-sm-4"></div> 
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4" style="color:#006600" id="signed_message"></div>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+        </div>     
+    </div>
+    <!-- --------------------------------- Footer --------------------------------- -->
+    <footer class="blog-footer">
+        <p>Powered by <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a></p>
+    </footer>
+    <script src="../js/bootstrap.min.js"></script>
+
+</body>
 </html>
