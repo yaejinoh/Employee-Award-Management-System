295064fe7de2ace260177e8b5559a78acb9e1a1e
yaejinoh <ohya@oregonstate.edu>
2017-05-07 04:09:36 -0400

    Update createAwardPDF.php

--- userforms/createAwardPDF.php
+++ userforms/createAwardPDF.php
@@ -56,7 +56,7 @@ if(!empty($_POST['export'])) {
 
         // draw jpeg image
         $border_info = getimagesize('../img/certificate-border.jpg');
-        $pdf->Image('../img/certificate-border.jpg', 0, 0, 200, "jpg");
+        $pdf->Image('../img/certificate-border.jpg', 0, 0, 300, "jpg");
        
         // restore full opacity
         $pdf->SetAlpha(1);
