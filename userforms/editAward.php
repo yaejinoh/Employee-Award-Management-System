340404d70a48097e70ee65fdf0eda3302b163641
yaejinoh <ohya@oregonstate.edu>
2017-05-27 04:04:22 -0400

    Update editAward.php

--- userforms/editAward.php
+++ userforms/editAward.php
@@ -381,7 +381,7 @@ if (!isset($_Session['employeeLastName']) && !isset($_SESSION['employeeLoggedIn'
 			$awardType = $_POST['awardType'];
 			$region = $_POST['region'];
 			$awardID = $_POST['awardID'];
-			echo $date . $time . $name . $awardType . $region .  $awardID;
+			echo "date: " . $date . "time: " . $time . "name: " . $name . "awardType: " . $awardType . "region: " . $region . "awardID: " . $awardID;
 			
 			if(!($stmt->bind_param("ssiiii", $_POST['date'], $_POST['time'], $_POST['name'], $_POST['awardType'], $_POST['region'], $_POST['awardID']))){
 				echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
