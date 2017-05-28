1850e5e14440cd830362ff422fbe3fe7293ac6c3
yaejinoh <ohya@oregonstate.edu>
2017-05-28 04:23:51 -0400

    Update createAwardPDFmail.php

--- userforms/createAwardPDFmail.php
+++ userforms/createAwardPDFmail.php
@@ -16,59 +16,6 @@ if (!isset($_Session['employeeLastName']) && !isset($_SESSION['employeeLoggedIn'
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <meta name="description" content="Delphinus Employee Recognition">
-        <meta name="author" content="Emmalee Jones, Yae Jin Oh">
-
-        <title>Employee Recognition Awards</title>
-
-        <!-- Bootstrap core CSS -->
-        <link href="../css/bootstrap.min.css" rel="stylesheet">
-        <!-- Custom styles for this template -->
-        <link href="../css/blog.css" rel="stylesheet">
-	<link href="../css/award.css" rel="stylesheet">
-        <script src="../js/jquery.min.js"></script>
-        <script src="../js/functions.js"></script>
-
-    </head>
-    <body>
-        <!-- --------------------------------- Navigation Bar --------------------------------- -->
-
-        <div class="blog-masthead">
-            <div class="container">
-                <nav class="blog-nav">
-                    <a class="navbar-brand" href="userLogout.php"> Employee Recognition Awards</a>  
-                    <form class="navbar-brand pull-right">
-                         <a> <?php echo "Employee Name:" . " " . $_SESSION['employeeFirstName']. " " . $_SESSION['employeeLastName'] ; ?> </a>
-                    </form>
-                    <!-- --------------------------------- Logout Form --------------------------------- -->
-                    <form class="navbar-form pull-right" method="POST" action="userLogout.php">
-                        <input type="submit" value = "Sign out" name="logout form)"> 
-                    </form>
-                </nav>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-            </div>
-        </div>
-        <div class="container">
-            <div class="row"> 
-                <br/>
-                <div class="col-sm-4"></div> 
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4" style="color:#FF0000" id="login_message"></div>
-                </br>
-            </div>     
-        </div>  
-
-        <!-- --------------------------------- Admin Sign In Form --------------------------------- -->
-        <div class="container" >
-            <h1>Award Creation</h1>
-            </br>
-            </br>
 
 <!-- --------------------------------- If the user submitted --SEND TO RECIPIENT-- button --------------------------------- -->
 <?php
@@ -209,41 +156,3 @@ if(!empty($_POST['export-mail'])) {
     $mysqli->close();
 ?>
 		      
-            </br> 
-            <a href="userMenu.php">User Menu</a>
-            </br>
-       </div> 
-        <div class="container">
-            <div class="row">   
-                <div class="col-sm-4"></div> 
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4" style="color:#006600" id="signed_message"></div>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-            </div>     
-        </div>
-        <!-- --------------------------------- Footer --------------------------------- -->
-        <footer class="blog-footer">
-            <p>Powered by <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a></p>
-        </footer>
-        <script src="../js/bootstrap.min.js"></script>
-
-    </body>
-</html>
