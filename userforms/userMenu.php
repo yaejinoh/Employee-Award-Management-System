99baa00e089560ff0a31b760be442bba3bfaec44
yaejinoh <ohya@oregonstate.edu>
2017-06-07 01:38:44 -0400

    Update userMenu.php

--- userforms/userMenu.php
+++ userforms/userMenu.php
@@ -1,12 +1,12 @@
 <?php
-#userMenu.php - CS467, Emmalee Jones, Yae Jin Oh 
-#User Menu  
+#awards.php - CS467, Emmalee Jones, Yae Jin Oh 
+#Create Awards 
 #Error Reporting Settings
 error_reporting(E_ALL);
 ini_set("display_errors", "ON");
 //Start PHP Session
 session_start();
-
+include '../phpmysql/connect.php';
 #Test for valid Session
 if (!isset($_Session['employeeLastName']) && !isset($_SESSION['employeeLoggedIn'])) {
     $_SESSION = array();
@@ -31,7 +31,7 @@ if (!isset($_Session['employeeLastName']) && !isset($_SESSION['employeeLoggedIn'
         <link href="../css/bootstrap.min.css" rel="stylesheet">
         <!-- Custom styles for this template -->
         <link href="../css/blog.css" rel="stylesheet">
-        <link href="../css/award.css" rel="stylesheet">
+	<link href="../css/award.css" rel="stylesheet">
         <script src="../js/jquery.min.js"></script>
         <script src="../js/functions.js"></script>
 
@@ -44,7 +44,7 @@ if (!isset($_Session['employeeLastName']) && !isset($_SESSION['employeeLoggedIn'
                 <nav class="blog-nav">
                     <a class="navbar-brand" href="userLogout.php"> Employee Recognition Awards</a>  
                     <form class="navbar-brand pull-right">
-                        <a> <?php echo "Employee Name:" . " " . $_SESSION['employeeFirstName'] . " " . $_SESSION['employeeLastName']; ?> </a>
+                         <a> <?php echo "Employee Name:" . " " . $_SESSION['employeeFirstName']. " " . $_SESSION['employeeLastName'] ; ?> </a>
                     </form>
                     <!-- --------------------------------- Logout Form --------------------------------- -->
                     <form class="navbar-form pull-right" method="POST" action="userLogout.php">
@@ -53,7 +53,6 @@ if (!isset($_Session['employeeLastName']) && !isset($_SESSION['employeeLoggedIn'
                 </nav>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
             </div>
         </div>
-
         <div class="container">
             <div class="row"> 
                 <br/>
@@ -64,21 +63,95 @@ if (!isset($_Session['employeeLastName']) && !isset($_SESSION['employeeLoggedIn'
             </div>     
         </div>  
 
-        <!-- --------------------------------- user Menu Form --------------------------------- -->
+        <!-- --------------------------------- Admin Sign In Form --------------------------------- -->
         <div class="container" >
-            <h1>User Menu</h1>
+            <h1>Award Creation</h1>
             </br>
             </br>
-            <form action="awards.php" class="menu-button">
-                <input type="submit" value="Awards" />
-            </form>
-            <form action="delAwards.php" class="menu-button">
-                <input type="submit" value="Delete Awards" />
-            </form>
-            <form action="userMod.php" class="menu-button">
-                <input type="submit" value="Modify Profile" />
-            </form>
 
+        <!-- --------------------------------- Award Creation Form --------------------------------- -->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-3">
+			</div>
+			<div class="col-lg-6">
+	<div id="award-body">
+	    <form method="post" action="createAward.php" id="award-form"> <!-- post to page handling form-->    
+                <fieldset>
+                    <legend> Create an Award Certificate </legend>
+                    <p>Name: 
+                        <select name="name"> 
+                            <?php
+                            // creates option for origin
+                            if(!($stmt = $mysqli->prepare("SELECT id, firstname, lastname, emailaddress FROM `Employees`"))){
+                                echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+                            }
+                            if(!$stmt->execute()){
+                                echo "Execute failed: " . $stmt->errno . " " . $stmt->error;
+                            }
+                            if(!$stmt->bind_result($id, $firstname, $lastname, $emailaddress)){
+                                echo "Bind failed: " . $stmt->errno . " " . $stmt->error;
+                            }
+                            while($stmt->fetch()){
+                                echo '<option value=" '. $id . ' "> ' . $firstname . ", " . $lastname . '</option>\n';
+                            }
+                            $stmt->close();
+                            ?>
+                        </select> </p>
+                    <p>Award Type: 
+                        <select name="awardType"> 
+                            <?php
+                            // creates option for origin
+                            if(!($stmt = $mysqli->prepare("SELECT ctid, type FROM `CertType`"))){
+                                echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+                            }
+                            if(!$stmt->execute()){
+                                echo "Execute failed: " . $stmt->errno . " " . $stmt->error;
+                            }
+                            if(!$stmt->bind_result($ctid, $type)){
+                                echo "Bind failed: " . $stmt->errno . " " . $stmt->error;
+                            }
+                            while($stmt->fetch()){
+                                echo '<option value=" '. $ctid . ' "> ' . $type . '</option>\n';
+                            }
+                            $stmt->close();
+                            ?>
+                        </select>
+                    </p>
+                    <p>Region: 
+                        <select name="region"> 
+                            <?php
+                            // creates option for origin
+                            if(!($stmt = $mysqli->prepare("SELECT rid, sector FROM `Regions`"))){
+                                echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+                            }
+                            if(!$stmt->execute()){
+                                echo "Execute failed: " . $stmt->errno . " " . $stmt->error;
+                            }
+                            if(!$stmt->bind_result($rid, $sector)){
+                                echo "Bind failed: " . $stmt->errno . " " . $stmt->error;
+                            }
+                            while($stmt->fetch()){
+                                echo '<option value=" '. $rid . ' "> ' . $sector . '</option>\n';
+                            }
+                            $stmt->close();
+                            ?>
+                        </select>
+                    </p>
+                    <p>
+                        <input type="submit" name="add" value="Create Award">
+                        <input type="submit" name="view" value="View All Awards">
+                    </p>
+                </fieldset>
+        </form>
+	</div>
+		</div>
+	</div>
+
+
+            </br> 
+            <a href="userMenu.php">User Menu</a>
+            </br>
         </div>
         <div class="container">
             <div class="row">   
